681fbcc2f83ccbcc9284f31aeaa4849c37110cf2
unknown <hydra2medusa@gmail.com>
2024-10-14 12:57:44 +0800

    editing

--- Phoenix-project/app/Http/Middleware/Shalguur.php
+++ Phoenix-project/app/Http/Middleware/Shalguur.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class Shalguur
@@ -15,6 +16,7 @@ class Shalguur
      */
     public function handle(Request $request, Closure $next): Response
     {
+
         return $next($request);
     }
 }
