e34a37822274d31b5f5de285348179112270d117
6Tushig9 <b.tushig21@gmail.com>
2024-11-28 00:30:08 +0800

    statistics edit

--- Phoenix-project/database/migrations/2024_09_24_142353_create_statistics_table.php
+++ Phoenix-project/database/migrations/2024_09_24_142353_create_statistics_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->mediumInteger('year')->nullable()->default(0);
             $table->mediumInteger('household_sum')->nullable()->default(0);
-            $table->mediumText('saved_bill')->nullable()->default(0);
-            $table->mediumText('carbon_reduced')->nullable()->default(0);
+            $table->mediumText('saved_bill')->nullable();
+            $table->mediumText('carbon_reduced')->nullable();
             $table->timestamps();
         });
     }
